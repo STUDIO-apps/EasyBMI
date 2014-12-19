e66682e62b8bd86f3e7d9f630f2370e84e0c6cd6
zacdevil10 <zac1999ad@gmail.com>
2014-12-19 22:28:51 +0000

    All basic functions
    Added scale and history page. Scale onClick events open a detail
    activity.

--- mobile/src/main/java/uk/co/appsbystudio/easybmi/MainActivity.java
+++ mobile/src/main/java/uk/co/appsbystudio/easybmi/MainActivity.java
@@ -1,8 +1,10 @@
 package uk.co.appsbystudio.easybmi;
 
 import android.app.ActionBar;
+import android.app.Activity;
 import android.app.FragmentTransaction;
 import android.app.LoaderManager;
+import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
 import android.support.v4.app.Fragment;
@@ -18,9 +20,14 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.EditorInfo;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.GridView;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
@@ -29,19 +36,13 @@ import java.util.Locale;
 import uk.co.appsbystudio.easybmi.SQL.DBHelper;
 import uk.co.appsbystudio.easybmi.SQL.HistoryRepo;
 import uk.co.appsbystudio.easybmi.SQL.SQL_History_table;
+import uk.co.appsbystudio.easybmi.Scale_Info.scale_detail;
 
 
 public class MainActivity extends ActionBarActivity implements android.support.v7.app.ActionBar.TabListener {
 
     SectionsPagerAdapter mSectionsPagerAdapter;
     ViewPager mViewPager;
-    private DBHelper dbHelper;
-    EditText weight_entry = (EditText) findViewById(R.id.weight_entry);
-    EditText height_entry = (EditText) findViewById(R.id.height_entry);
-    TextView weight_label = (TextView) findViewById(R.id.weight_label);
-    TextView height_label = (TextView) findViewById(R.id.height_label);
-    TextView calc_label = (TextView) findViewById(R.id.total_label);
-    Button calculate = (Button) findViewById(R.id.calculate);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,6 +69,28 @@ public class MainActivity extends ActionBarActivity implements android.support.v
                     actionBar.newTab()
                             .setText(mSectionsPagerAdapter.getPageTitle(i))
                             .setTabListener(this));
+        }
+    }
+
+    public void onClick(View view){
+        EditText weight_entry = (EditText) findViewById(R.id.weight_entry);
+        EditText height_entry = (EditText) findViewById(R.id.height_entry);
+        TextView weight_label = (TextView) findViewById(R.id.weight_label);
+        TextView height_label = (TextView) findViewById(R.id.height_label);
+        TextView calc_label = (TextView) findViewById(R.id.total_label);
+        Button calculate = (Button) findViewById(R.id.calculate);
+        String check_1 = weight_entry.getText().toString();
+        String check_2 = height_entry.getText().toString();
+
+        if (check_1.matches("")|| check_2.matches("")){
+            return;
+        } else {
+            float t;
+            float w = Float.parseFloat(weight_entry.getText().toString());
+            float h = Float.parseFloat(height_entry.getText().toString());
+
+            t = (float) ((double)Math.round(10D * (double)(w / (h * h))) / 10D);
+            calc_label.setText("Your BMI is: " + Float.toString(t));
         }
     }
 
@@ -152,19 +175,95 @@ public class MainActivity extends ActionBarActivity implements android.support.v
 
     public static class Scale extends Fragment {
 
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_scale, container, false);
-            return rootView;
-        }
-    }
+        private AbsListView mListView;
+        private ListAdapter mAdapter;
 
-    public static class History extends Fragment {
+        public Scale() {
+
+        }
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            String[] scale_values = new String[] {"Very severely underweight", "Severely underweight", "Underweight", "Normal", "Overweight", "Obese"};
+
+            mAdapter = new ArrayAdapter<String>(getActivity(),
+                    android.R.layout.simple_list_item_1, android.R.id.text1, scale_values);
+        }
+
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_history, container, false);
-            return rootView;
+            View view = inflater.inflate(R.layout.fragment_scale, container, false);
+
+            // Set the adapter
+            mListView = (AbsListView) view.findViewById(android.R.id.list);
+            ((AdapterView<ListAdapter>) mListView).setAdapter(mAdapter);
+
+            mListView.setOnItemClickListener(new itemSelected());
+
+            return view;
         }
+
+        private class  itemSelected implements  ListView.OnItemClickListener{
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                selectItem(position);
+            }
+        }
+
+        public void selectItem(int position) {
+            TextView detail = (TextView) getView().findViewById(R.id.detail);
+            Intent intent = new Intent(getActivity(), scale_detail.class);
+            switch (position) {
+                case 0:
+                    startActivity(intent);
+                case 1:
+                    startActivity(intent);
+                case 2:
+                    startActivity(intent);
+                case 3:
+                    startActivity(intent);
+                case 4:
+                    startActivity(intent);
+                case 5:
+                    startActivity(intent);
+            }
+        }
+
+    }
+
+    public static class History extends Fragment {
+
+        private AbsListView mListView;
+        private ListAdapter mAdapter;
+
+        public History() {
+
+        }
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            String[] values = new String[] {"1", "2", "3", "4"};
+
+            mAdapter = new ArrayAdapter<String>(getActivity(),
+                    android.R.layout.simple_list_item_1, android.R.id.text1, values);
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View view = inflater.inflate(R.layout.fragment_history, container, false);
+
+            // Set the adapter
+            mListView = (AbsListView) view.findViewById(android.R.id.list);
+            ((AdapterView<ListAdapter>) mListView).setAdapter(mAdapter);
+
+            return view;
+        }
+
     }
 }
---
+++ mobile/src/main/java/uk/co/appsbystudio/easybmi/Scale_Info/scale_detail.java
@@ -0,0 +1,17 @@
+package uk.co.appsbystudio.easybmi.Scale_Info;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+import uk.co.appsbystudio.easybmi.R;
+
+/**
+ * Created by Zac on 19/12/2014.
+ */
+public class scale_detail extends ActionBarActivity{
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.detail_scale);
+    }
+}
